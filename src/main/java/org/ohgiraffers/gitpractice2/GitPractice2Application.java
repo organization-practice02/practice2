03d59b80e6d43475f2bb24bd344d7f131806b015
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-02-16 11:39:19 +0900

    first commit

---
+++ src/main/java/org/ohgiraffers/gitpractice2/GitPractice2Application.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.gitpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ohgiraffers/gitpractice2/GitPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.gitpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
